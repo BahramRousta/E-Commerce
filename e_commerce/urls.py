056f49bf0fab9e8ab8cc5c5c196a9ebd0fb6d636
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-10-15 22:13:42 +0330

    change urls for deploy

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-              static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+              #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
