468b237d7082bf9d5cb61dbc02d9e6ffdb416df8
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-02-01 11:54:09 +0330

    add AdminSite test

--- BookStoreApp/book/tests.py
+++ BookStoreApp/book/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import time
+from selenium import webdriver
+from django.test import LiveServerTestCase
+from selenium.webdriver.common.by import By
 
-# Create your tests here.
+
+class AdminTest(LiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_admin_site(self):
+        self.browser.get("http://127.0.0.1:8000/admin/")
+        time.sleep(1)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('Django administration', body.text)
+
+
--- BookStoreApp/e_commerce/settings.py
+++ BookStoreApp/e_commerce/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BookStore',
-        'USER': 'postgres',
-        'PASSWORD': 'bahram1371',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
