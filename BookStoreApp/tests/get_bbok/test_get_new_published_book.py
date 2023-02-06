3e36f053711d5bf54965054a149f163c40c21008
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-02-06 23:35:02 +0330

    modify find element by xpath in test_get_best_seller_book

--- BookStoreApp/tests/get_bbok/test_get_new_published_book.py
+++ BookStoreApp/tests/get_bbok/test_get_new_published_book.py
@@ -6,17 +6,17 @@ class Test_GetBestSellerBook(TestBaseConfig):
 
     def test_get_best_seller_book(self):
 
-        book_title = self.driver.find_element(By.XPATH, '//*[@id="recommended-item-carousel"]/div/div/div/div/div/div/h2')
+        book_title = self.driver.find_element(By.XPATH, '((//*[@class="item active"])[2]/child::*)[1]//h2')
+
         book_title_text = book_title.text
 
         get_book_btn = self.driver.find_element(By.XPATH, "//*[text()='مشاهده کتاب']")
         get_book_btn.click()
 
-        book_title_target = self.driver.find_element(By.XPATH, '/html/body/section/div/div/div[1]/div[1]/div[2]/div/div[1]/h2')
+        book_title_target = self.driver.find_element(By.XPATH, '(//*[@class="product-information"])/child::*[2]/h2')
         book_title_target_text = book_title_target.text
 
         assert book_title_text == book_title_target_text
 
-        add_to_card_btn = self.driver.find_element(By.XPATH, "/html/body/section/div/div/div[1]/div[1]/div[2]/div/div[2]/form/span/button")
-
-        assert "افـزودن به سبـد خریـد" in add_to_card_btn.text
+        add_to_card_btn = self.driver.find_element(By.XPATH, '//*[text()="افـزودن به سبـد خریـد"]')
+        assert 'افـزودن به سبـد خریـد' in add_to_card_btn.text
