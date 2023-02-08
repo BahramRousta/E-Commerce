d41ad30c0b042da076c8b4caac6297ffd3c045ca
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-02-08 14:59:57 +0330

    add unit test

--- BookStoreApp/tests/book/test_book_detail.py
+++ BookStoreApp/tests/book/test_book_detail.py
@@ -1,19 +1,31 @@
 import pytest
 from django.urls import reverse
+from django.utils import timezone
+
+from book.models import Book
 
 
 class TestBookDetail:
 
+    def setup_method(self):
+        self.base_url = "book:book_detail"
+
     @pytest.mark.django_db
     def test_book_detail_page(self, client, book):
-        base_url = reverse("book:book_detail", kwargs={"slug": book.slug})
 
-        response = client.get(base_url)
+        self.detail_url = reverse(self.base_url, kwargs={"slug": book.slug})
+        response = client.get(self.detail_url)
 
         assert response.status_code == 200
         assert response.context["request"].path == f"/book_detail/{book.slug}/"
         assert response.templates[0].name == "book/book_detail.html"
 
+    @pytest.mark.django_db
+    def test_book_detail_page_object(self, client, book):
 
+        self.detail_url = reverse(self.base_url, kwargs={"slug": book.slug})
+        response = client.get(self.detail_url)
 
+        assert response.context["book"] == book
+        assert response.context["book_categories"] == book.category
 
