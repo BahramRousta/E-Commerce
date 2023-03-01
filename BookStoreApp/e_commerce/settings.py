fa7727dbc0d12d339416b1933767dc05eb3b0fe5
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-03-01 22:40:07 +0330

    delete cache middleware

--- BookStoreApp/e_commerce/settings.py
+++ BookStoreApp/e_commerce/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'book.middleware.cache_view'
 ]
 
 INTERNAL_IPS = [
