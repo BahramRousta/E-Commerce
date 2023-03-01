533805217d26c18e3023cbaeb5eebccc450a133e
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-03-01 22:39:46 +0330

    delete tag url

--- BookStoreApp/book/urls.py
+++ BookStoreApp/book/urls.py
@@ -13,7 +13,7 @@ app_name = 'book'
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('tag/<int:tag_id>/', BookListByTag.as_view(), name='book_list_by_tag'),
+    path('tag/<int:tag_id>/', BookListByTag, name='book_list_by_tag'),
     path('book_detail/<slug:slug>/', book_detail, name='book_detail'),
     path('favorite_book/<int:id>/', favorite_book, name='add_favorite_book'),
     path('favorites_book/', FavoriteBooks.as_view(), name='favorites_book'),
