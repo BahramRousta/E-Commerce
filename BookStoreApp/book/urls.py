beaf6daeace47486423d5dec7352722dad7fcc85
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-21 22:46:55 +0330

    refactor BookListByTag to class view

--- BookStoreApp/book/urls.py
+++ BookStoreApp/book/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 from .views import (
-    home_page,
+    HomeView,
     book_detail,
-    BookListByTag,
     FavoriteBooks,
     favorite_book,
     main_search,
-    remove_favorite_book
+    remove_favorite_book,
+    BookListByTag
 )
 
 app_name = 'book'
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
-    path('tag/<int:tag_id>/', BookListByTag, name='book_list_by_tag'),
+    path('', HomeView.as_view(), name='home_page'),
+    path('tag/<int:tag_id>/', BookListByTag.as_view(), name='book_list_by_tag'),
     path('book_detail/<slug:slug>/', book_detail, name='book_detail'),
     path('favorite_book/<int:id>/', favorite_book, name='add_favorite_book'),
     path('favorites_book/', FavoriteBooks.as_view(), name='favorites_book'),
