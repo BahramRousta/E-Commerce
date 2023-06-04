c8f789fcca8424bb44d3e8789f28ac414b60505d
bahramrousta <rousta.bahram@gmail.com>
2023-06-04 18:50:06 +0330

    modify profile admin panel

--- BookStoreApp/accounts/admin.py
+++ BookStoreApp/accounts/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('user', 'email', 'city', 'province')
+    list_filter = ('city', 'province')
+    search_fields = ('user',)
+    ordering = ('user',)
+    list_editable = ('city', 'province')
+
+
+admin.site.register(Profile, ProfileAdmin)
